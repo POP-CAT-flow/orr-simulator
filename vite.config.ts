6367001222ca35f8a64459dd3693d67462d18b5a
POP-CAT-flow <minyuan2023@gmail.com>
2026-02-26 10:02:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    // 解决 404 报错的关键
-    base: '/orr-simulator/',
-
-    plugins: [react()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        // 因为没有 src 文件夹，这里指向当前目录
-        '@': path.resolve(__dirname, '.'),
-      }
-    },
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
+export default defineConfig({
+  // 必须与 GitHub 仓库名一致
+  base: '/orr-simulator/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // 扁平结构别名指向根目录
+      '@': path.resolve(__dirname, '.'),
     }
-  };
-});
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
